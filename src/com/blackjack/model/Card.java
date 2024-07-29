5d18903f3cfbc8687ef3a977143931321d3c87aa
Aranka Bullen <arankab@tlgcohort.com>
2024-07-29 13:03:45 -0400

    added clubs to card enum

--- src/com/blackjack/model/Card.java
+++ src/com/blackjack/model/Card.java
@@ -3,5 +3,10 @@ package com.blackjack.model;
 enum Card {
     ACE_SPADES,
     TWO_SPADES,
-    THREE_SPADES;
+    THREE_SPADES,
+
+    //alice work below:
+    ACE_CLUB,
+    TWO_CLUB,
+    THREE_CLUB;
 }
